03104b076b47f836d4cb88801602451a2b1467ab
valverdecristian <crizthian2010@gmail.com>
2025-08-26 12:34:35 -0300

    solucion de ruteo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,14 +3,15 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
+    redirectTo: 'login',
+    pathMatch: 'full',
   },
   {
     path: 'login',
     loadComponent: () => import('./login/login.page').then((m) => m.LoginPage),
   },
   {
-    path: 'productos',
-    loadComponent: () => import('./productos/productos.page').then( m => m.ProductosPage)
+    path: 'tabs',
+    loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
   },
 ];
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { RouterLink } from '@angular/router';
+import { Router } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
@@ -9,10 +9,30 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule, RouterLink],
+  imports: [IonicModule, CommonModule, FormsModule],
 })
 export class LoginPage implements OnInit {
-  constructor() {}
+  email = '';
+  password = '';
+  error = '';
+  loading = false;
+
+  constructor(private router: Router) {}
+
+  login() {
+    this.loading = true;
+    this.error = '';
+
+    // Simulate an async login operation
+    setTimeout(() => {
+      this.loading = false;
+      if (this.email === 'test@test.com' && this.password === '123456') {
+        this.router.navigate(['/tabs']);
+      } else {
+        this.error = 'Invalid email or password';
+      }
+    }, 1000);
+  }
 
   ngOnInit() {}
 }
